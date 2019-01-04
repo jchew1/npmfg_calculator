ab4d594d75dc431fb44ffb55253899b9bb8370ed
Jonathan Chew <jchew513@gmail.com>
2019-01-03 16:33:43 -0800

    button width fixed need to fix height activity state saved implemented unit change buttons refactor conversion methods

--- app/src/main/java/com/example/jonathan/npmfg_calculator/MainActivity.java
+++ app/src/main/java/com/example/jonathan/npmfg_calculator/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.jonathan.npmfg_calculator;
 
+import android.graphics.Point;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
@@ -15,109 +19,243 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "ABC";
     private static final int numButtonsX = 4;
-    private static final int numButtonsY = 4;
-    private static final String[] BUTTON_LABELS = new String[]{"7", "8", "9", "in", "4", "5", "6", "", "1", "2", "3", "mm", "0", ",", "C", ""};
-    TextView topNum;
-    TextView bottomNum;
-    boolean containsDot = false;
+    private static final int numButtonsY = 5;
+    private static final String[] units = new String[]{"in", "mm", "cm", "ft"};
+    TextView topNumView;
+    TextView bottomNumView;
+    TextView unit1View;
+    TextView unit2View;
+    Button unit1Button;
+    Button unit2Button;
+    int unit1Index = 0;
+    int unit2Index = 1;
+    boolean hasDot = false;
+    char hasOperator;
     DecimalFormat df = new DecimalFormat("#.####");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        topNum = findViewById(R.id.top_num);
-        bottomNum = findViewById(R.id.bottom_num);
-        Log.d(TAG, "oncreateabc");
+        topNumView = findViewById(R.id.top_num);
+        bottomNumView = findViewById(R.id.bottom_num);
+        unit1View = findViewById(R.id.unit_1);
+        unit2View = findViewById(R.id.unit_2);
+        unit1View.setText(units[unit1Index]);
+        unit2View.setText(units[unit2Index]);
+        unit1Button = findViewById(R.id.button_unit1);
+        unit2Button = findViewById(R.id.button_unit2);
+        unit1Button.setText(units[unit1Index]);
+        unit2Button.setText(units[unit2Index]);
+
+        if(savedInstanceState != null){
+            topNumView.setText(savedInstanceState.getString("topNumViewText"));
+            bottomNumView.setText(savedInstanceState.getString("bottomNumViewText"));
+            hasDot =  savedInstanceState.getBoolean("hasDot");
+            hasOperator = savedInstanceState.getChar("hasOperator");
+            unit1Index = savedInstanceState.getInt("unit1Index");
+            unit2Index = savedInstanceState.getInt("unit2Index");
+            unit1View.setText(units[unit1Index]);
+            unit2View.setText(units[unit2Index]);
+            unit1Button.setText(units[unit1Index]);
+            unit2Button.setText(units[unit2Index]);
+        }
+
+        Log.d(TAG, "oncreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
         metricsSetup();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("topNumViewText", topNumView.getText().toString());
+        outState.putString("bottomNumViewText", bottomNumView.getText().toString());
+        outState.putBoolean("hasDot", hasDot);
+        outState.putChar("hasOperator", hasOperator);
+        outState.putInt("unit1Index", unit1Index);
+        outState.putInt("unit2Index", unit2Index);
     }
 
     private void metricsSetup(){
         DisplayMetrics displayMetrics= new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int width = displayMetrics.widthPixels/4;
+        int width = displayMetrics.widthPixels/numButtonsX;
+
         GridLayout grid = findViewById(R.id.buttons);
-        Log.d(TAG, String.valueOf(grid.getUseDefaultMargins()));
-        Button v = null;
+        Button v;
         for(int i=0; i<grid.getChildCount(); i++){
             v = (Button)grid.getChildAt(i);
-            v.setWidth(width);
             GridLayout.LayoutParams params = (GridLayout.LayoutParams) v.getLayoutParams();
             params.bottomMargin = params.topMargin = params.leftMargin = params.rightMargin = 0;
+            params.width = width;
             v.setLayoutParams(params);
-            Log.d(TAG, "leftmargin: " + ((GridLayout.LayoutParams) v.getLayoutParams()).leftMargin);
         }
-//        Log.d(TAG, "metricssetup " + String.valueOf(width));
-////        <Button
-////        android:id="@+id/button0"
-////        android:text="@string/zero"
-////        android:onClick="putNumber"
-////        android:layout_row="3"
-////        android:layout_column="0"/>
-//        for(int i=0; i<numButtonsY; i++){
-//            for(int j=0; j<numButtonsX; j++){
-//                Log.d(TAG, "metricssetup: " + BUTTON_LABELS[i*numButtonsX + j]);
-//                Button b = new Button(this);
-//                GridLayout.LayoutParams params = new GridLayout.LayoutParams(GridLayout.spec(j), GridLayout.spec(i));
-//                params.bottomMargin = params.topMargin = params.leftMargin = params.rightMargin = 0;
-//                b.setId(this.getResources().getIdentifier("button"+BUTTON_LABELS[i*numButtonsX + j], "string", this.getPackageName()));
-//                b.setLayoutParams(params);
-//                b.setWidth(width);
-//                b.setText(BUTTON_LABELS[i*numButtonsX + j]);
-//                grid.addView(b);
-//            }
-//        }
     }
 
     public void putNumber(View view) {
         String num = ((TextView)view).getText().toString();
-        String currentNum = bottomNum.getText().toString();
+        String currentNum = bottomNumView.getText().toString();
         if(currentNum.equals("0")){
-            bottomNum.setText(num);
+            bottomNumView.setText(num);
         }else{
-            bottomNum.setText(currentNum + num);
+            currentNum += num;
+            bottomNumView.setText(currentNum);
         }
         convertNum();
     }
 
     public void putDot(View view) {
-        if(!containsDot){
-            String currentNum = bottomNum.getText().toString();
-            bottomNum.setText(currentNum + ".");
-            containsDot = true;
+        if(!hasDot){
+            String currentNum = bottomNumView.getText().toString() + ".";
+            bottomNumView.setText(currentNum);
+            hasDot = true;
+        }
+    }
+
+    public void putOperator(View view){
+        String currentNum = bottomNumView.getText().toString();
+        if(hasOperator == '\u0000') {
+            currentNum += ((Button)view).getText().toString();
+            bottomNumView.setText(currentNum);
+            hasOperator = currentNum.charAt(currentNum.length()-1);
+        }
+    }
+
+    public void equals(View view){
+        if(hasOperator != '\u0000'){
+            bottomNumView.setText(simplifyNum(bottomNumView.getText().toString()));
+            hasOperator = '\u0000';
         }
     }
 
     public void clearNum(View view) {
-        topNum.setText("0");
-        bottomNum.setText("0");
-        containsDot = false;
-    }
-
-    private void convertNum() {
-        String unit1 = ((TextView)findViewById(R.id.unit_1)).getText().toString();
-        TextView unit2View = findViewById(R.id.unit_2);
-        String unit2 = unit2View.getText().toString();
-        String num1 = bottomNum.getText().toString();
-        String newNum = topNum.getText().toString();
-        switch(unit2){
-            case "in":
-                if(unit1.equals("mm")){
-                    newNum = String.valueOf(df.format(inToMm(Double.valueOf(num1), false)));
-                }
-                break;
-            case "mm":
-                if(unit1.equals("in")){
-                    newNum = String.valueOf(df.format(inToMm(Double.valueOf(num1), true)));
-                }
-        }
-        topNum.setText(newNum);
-    }
-
-    private double inToMm(double num1, boolean reverse){
-        return reverse ? num1/25.4 : num1*25.4;
+        topNumView.setText("0");
+        bottomNumView.setText("0");
+        hasDot = false;
+        hasOperator = '\u0000';
     }
 
     public void switchUnit(View view) {
+        int id = view.getId();
+        if(id == R.id.button_unit1){
+            unit1Index++;
+            unit1Index = unit1Index >= units.length ? 0 : unit1Index;
+            unit1Button.setText(units[unit1Index]);
+            unit1View.setText(units[unit1Index]);
+        }else{
+            unit2Index++;
+            unit2Index = unit2Index >= units.length ? 0 : unit2Index;
+            unit2Button.setText(units[unit2Index]);
+            unit2View.setText(units[unit2Index]);
+        }
+        convertNum();
     }
+
+    private void convertNum() {
+        String unit1 = unit1View.getText().toString();
+        String unit2 = unit2View.getText().toString();
+        String num1 = bottomNumView.getText().toString();
+        String newNum = topNumView.getText().toString();
+        String temp = hasOperator == '\u0000' ? num1 : simplifyNum(num1);
+
+        switch(unit2){
+            case "in":
+                newNum = convertIn(Double.valueOf(temp), unit1);
+                break;
+            case "mm":
+                newNum = convertMm(Double.valueOf(temp), unit1);
+                break;
+        }
+        topNumView.setText(newNum);
+    }
+
+    private String simplifyNum(String expr){
+        String[] nums = expr.split(String.valueOf(hasOperator));
+        Double result = 0.0;
+
+        switch(hasOperator){
+            case '+':
+                result = Double.parseDouble(nums[0]) + Double.parseDouble(nums[1]);
+                break;
+            case '-':
+                result = Double.parseDouble(nums[0]) - Double.parseDouble(nums[1]);
+                break;
+            case 'x':
+                result = Double.parseDouble(nums[0]) * Double.parseDouble(nums[1]);
+                break;
+            case '/':
+                result = Double.parseDouble(nums[0]) / Double.parseDouble(nums[1]);
+                break;
+        }
+        return String.valueOf(result);
+    }
+
+    private String convertIn(double num, String unit){
+        Double result = num;
+        switch(unit){
+            case "mm":
+                result = num*25.4;
+                break;
+            case "cm":
+                result = num*2.54;
+                break;
+            case "ft":
+                result = num/12;
+                break;
+        }
+        return String.valueOf(df.format(result));
+    }
+
+    private String convertMm(double num, String unit){
+        Double result = num;
+        switch(unit){
+            case "in":
+                result = num/25.4;
+                break;
+            case "cm":
+                result = num/10;
+                break;
+            case "ft":
+                result = num/304.8;
+                break;
+        }
+        return String.valueOf(df.format(result));
+    }
+
+    private String convertCm(double num, String unit){
+        Double result = num;
+        switch(unit){
+            case "in":
+                result = num/2.54;
+                break;
+            case "mm":
+                result = num*10;
+                break;
+            case "ft":
+                result = num*30.48;
+                break;
+        }
+        return String.valueOf(df.format(result));
+    }
+
+    private String convertFt(double num, String unit){
+        Double result = num;
+        switch(unit){
+            case "in":
+                result = num*12;
+                break;
+                case "mm"
+        }
+    }
+
+    private double cmToIn(double num) {return num/2.54;}
+    private double cmToMm(double num) {return num*10;}
+    private double cmToFt(double num) {return num*30.48;}
+
 }
